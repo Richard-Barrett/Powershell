c9ca08ff8b418845205e378769170e7cf18c1c6c
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2019-02-28 23:12:01 -0600

    Create dhcp_check.ps1

---
+++ dhcp_check.ps1
@@ -0,0 +1,14 @@
+# Created by Richard Barrett
+# Date: Febraury 28 2019
+# Version: 1.0
+# Company: Cleardigitalmedia 
+# ============================================
+#               **Notes**
+# ============================================
+# Script must be ran as admin in Powershell
+# Script must be be set with execution policy
+# Policy must be Remote Signed
+# Policy will be determined and set
+# ============================================
+#             **Documentation**
+# ============================================
